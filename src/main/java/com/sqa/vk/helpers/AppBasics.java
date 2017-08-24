c06643443a14b5a986061bfca201ab259929ae93
Vladimir Kononov <vladimirkon@gmail.com>
2017-08-23 19:22:35 -0700

    Add more methods for primitive Java types

--- src/main/java/com/sqa/vk/helpers/AppBasics.java
+++ src/main/java/com/sqa/vk/helpers/AppBasics.java
@@ -22,8 +22,10 @@ public class AppBasics {
 
 	private static Scanner scanner = new Scanner(System.in);
 
-	public static void farewellUser(String userName) {
-		System.out.println("Thank you, " + userName + " for using our application.");
+	private static String userName;
+
+	public static void farewellUser() {
+		System.out.println("\nThank you, " + userName + " for using our application.");
 		System.out.println("Have a nice day!");
 	}
 
@@ -32,8 +34,35 @@ public class AppBasics {
 		System.out.println("Hello there! Wellcome to the " + appName + " Application");
 		// Ask the user for their name and store in a local scope String
 		// variable
+		// String userName = null;
 		System.out.print("Can i get your name? ");
-		return scanner.nextLine();
+		userName = scanner.nextLine();
+		return userName;
+	}
+
+	public static boolean requestBoolean(String question) {
+		String input;
+		System.out.println(question + " ");
+		input = scanner.nextLine();
+		if (input.trim().equalsIgnoreCase("Yes")) {
+			return true;
+		} else if (input.trim().equalsIgnoreCase("No")) {
+			return false;
+		} else {
+			System.out.println("You didn't respond to the question. Please enter \"Yes\" or \"No\"");
+			return false;
+		}
+	}
+
+	public static char requestChar(String question) {
+		char letter = 0;
+		System.out.println(question + " ");
+		try {
+			letter = scanner.nextLine().charAt(0);
+		} catch (StringIndexOutOfBoundsException e) {
+			System.out.println("You did not supply a valid input [" + letter + "]. please provide a single letter.");
+		}
+		return letter;
 	}
 
 	public static double requestDouble(String question) {
@@ -45,7 +74,6 @@ public class AppBasics {
 			num = Double.parseDouble(input);
 		} catch (NumberFormatException e) {
 			System.out.println("You did not supply a valid number [" + input + "]. please provide only digits.");
-			System.exit(0);
 		}
 		return num;
 	}
@@ -59,7 +87,6 @@ public class AppBasics {
 			num = Float.parseFloat(input);
 		} catch (NumberFormatException e) {
 			System.out.println("You did not supply a valid number [" + input + "]. please provide only digits.");
-			System.exit(0);
 		}
 		return num;
 	}
@@ -73,7 +100,32 @@ public class AppBasics {
 			num = Integer.parseInt(input);
 		} catch (NumberFormatException e) {
 			System.out.println("You did not supply a valid number [" + input + "]. please provide only digits.");
-			System.exit(0);
+		}
+		return num;
+	}
+
+	public static long requestLong(String question) {
+		String input;
+		long num = 0;
+		System.out.println(question + " ");
+		input = scanner.nextLine();
+		try {
+			num = Long.parseLong(input);
+		} catch (NumberFormatException e) {
+			System.out.println("You didn't provide expected input [" + input + "]. Please enter Long number.");
+		}
+		return num;
+	}
+
+	public static short requestShort(String question) {
+		String input;
+		short num = 0;
+		System.out.println(question + " ");
+		input = scanner.nextLine();
+		try {
+			num = Short.parseShort(input);
+		} catch (NumberFormatException e) {
+			System.out.println("You didn't provide expected input [" + input + "]. Please enter Short number.");
 		}
 		return num;
 	}
