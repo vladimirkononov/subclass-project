a56d1d8d37c2dc006c6ba4fc2cdbc02a987392d4
Vladimir Kononov <vladimirkon@gmail.com>
2017-08-24 18:41:14 -0700

    Create two exceptions

---
+++ src/main/java/com/sqa/vk/helpers/exceptions/RequestIntOverMaxException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: RequestIntOverMaxException.java<br>
+ *
+ *   Nepton, Jean-francois<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Aug 24, 2017
+ *   
+ */
+
+package com.sqa.vk.helpers.exceptions;
+
+
+/**
+ * RequestIntOverMaxException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Nepton, Jean-francois
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class RequestIntOverMaxException extends Exception {
+}
---
+++ src/main/java/com/sqa/vk/helpers/exceptions/RequestIntUnderMinException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: RequestIntUnderMinException.java<br>
+ *
+ *   Nepton, Jean-francois<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Aug 24, 2017
+ *   
+ */
+
+package com.sqa.vk.helpers.exceptions;
+
+
+/**
+ * RequestIntUnderMinException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Nepton, Jean-francois
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class RequestIntUnderMinException extends Exception {
+}
