0cb11a2264cabe1dc8b8bd00ffa3edb24c926bf4
Vladimir Kononov <vladimirkon@gmail.com>
2017-08-26 12:01:17 -0700

    Create greetUserAndIntroTest method

--- src/main/java/com/sqa/vk/helpers/AppBasics.java
+++ src/main/java/com/sqa/vk/helpers/AppBasics.java
@@ -40,6 +40,16 @@ public class AppBasics {
 		return userName;
 	}
 
+	public static void greetUserAndIntroTest(String appName, String testName) {
+		// Greet the user using a system out call
+		System.out.println("Welcome to the " + appName + " Application.");
+		// Ask the user for their name and store in a local scope String
+		// variable
+		System.out.println("\\-------------------------------------------------------------------\\");
+		System.out.println("You are running the following tests for " + testName);
+		System.out.println("\\-------------------------------------------------------------------\\\n");
+	}
+
 	public static boolean requestBoolean(String question) {
 		String input;
 		boolean isValid = false;
